00fb503015b444cbef5beb93f483890ff48dbf5a
Darren Roscoe <droscoe@clypd.com>
2017-01-09 17:57:48 -0500

    Add use of function mocks in song component test

--- src/components/Song.test.js
+++ src/components/Song.test.js
@@ -6,6 +6,8 @@ import ConnectedSong,  { Song } from './Song';
 // TODO add spies to mock callbacks
 
 describe('Song component', () => {
+  const play = jest.fn();
+  const pause = jest.fn();
   describe('Presentational component', () => {
     const testTrack = {
       name: "Testing track",
@@ -23,22 +25,9 @@ describe('Song component', () => {
       idx: 1,
       track: testTrack,
       isPlaying: false,
-      play: (track) => {
-        playCalls++;
-        playCallArgs.push(track);
-      },
-      pause: () => {
-        pauseCalls++;
-      }
+      play,
+      pause
     }
-    let playCalls = 0;
-    let playCallArgs = [];
-    let pauseCalls = 0;
-    beforeEach(() => {
-      playCalls = 0;
-      playCallArgs = [];
-      pauseCalls = 0;
-    })
     it('should render a table row with data', () => {
       const wrapper = shallow(<Song { ...testProps } />);
       expect(wrapper.is('tr')).toBe(true);
@@ -48,11 +37,10 @@ describe('Song component', () => {
     });
     it('should call play when the playpause button is pressed and the song is paused', () => {
       const wrapper = shallow(<Song { ...testProps } />);
-      expect(playCalls).toBe(0);
-      expect(playCallArgs.length).toBe(0);
+      expect(play.mock.calls.length).toBe(0);
       wrapper.find('button').simulate('click');
-      expect(playCalls).toBe(1);
-      expect(playCallArgs[0]).toBe(testTrack);
+      expect(play.mock.calls.length).toBe(1);
+      expect(play.mock.calls[0][0]).toBe(testTrack);
     })
     it('should call pause when the playpause button is pressed and the song is playing', () => {
       const pausedProps = {
@@ -60,9 +48,9 @@ describe('Song component', () => {
         isPlaying: true
       }
       const wrapper = shallow(<Song { ...pausedProps } />);
-      expect(pauseCalls).toBe(0);
+      expect(pause.mock.calls.length).toBe(0);
       wrapper.find('button').simulate('click');
-      expect(pauseCalls).toBe(1);
+      expect(pause.mock.calls.length).toBe(1);
     })
   })
   describe('connected component', () => {
